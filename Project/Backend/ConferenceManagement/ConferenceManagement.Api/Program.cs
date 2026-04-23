b7d094821be6a1236cdd3741beba431814884f33
akanlic2 <akanlic2@etf.unsa.ba>
2026-04-23 23:28:34 +0200

    App containerized

--- Project/Backend/ConferenceManagement/ConferenceManagement.Api/Program.cs
+++ Project/Backend/ConferenceManagement/ConferenceManagement.Api/Program.cs
@@ -1,3 +1,4 @@
+using ConferenceManagement.Api.Extensions;
 using ConferenceManagement.Application.Interfaces;
 using ConferenceManagement.Application.Services;
 using ConferenceManagement.Dal;
@@ -9,20 +10,38 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        npgsqlOptions =>
+        {
+            npgsqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(10),
+                errorCodesToAdd: null);
+        }));
 
 builder.Services.AddScoped<IConferenceRepository, ConferenceRepository>();
 builder.Services.AddScoped<IConferenceService, ConferenceService>();
 
 var app = builder.Build();
 
+var runMigrationsOnly =
+    builder.Configuration.GetValue<bool>("RUN_MIGRATIONS_ONLY");
+
+if (runMigrationsOnly)
+{
+    await app.Services.WaitForDatabaseAndApplyMigrationsAsync(app.Logger);
+    app.Logger.LogInformation("Migrations finished successfully. Exiting migrator container.");
+    return;
+}
+
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-   // app.UseSwaggerUI();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
